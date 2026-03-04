f89c6f99b255dda0ffc213014189fcdb830d378b
qualiasolutions <info@qualiasolutions.net>
2026-03-04 20:19:16 +0200

    feat(05-01): enable smooth dark mode transitions
    - Remove disableTransitionOnChange prop from ThemeProvider
    - Theme toggle now triggers smooth color transitions
    - Colors transition over ~300ms instead of instant flash
    - Requires companion CSS transition rules (next task)

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,16 +1,11 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import * as React from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
       {children}
     </NextThemesProvider>
   );
